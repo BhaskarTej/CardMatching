9832bd646e2ff28f9fcbfeda123897b20f40e30c
BhaskarTej <bhaskarteja02@gmail.com>
2024-10-14 15:34:58 -0400

    Second Commit User Interference

--- lib/main.dart
+++ lib/main.dart
@@ -10,3 +10,44 @@ void main() {
     ),
   );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Matching Game'),
+        ),
+        body: const GameBoard(),
+      ),
+    );
+  }
+}
+
+class GameBoard extends StatelessWidget {
+  const GameBoard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final gameState = Provider.of<GameState>(context);
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4, // 4x4 grid
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+      ),
+      padding: const EdgeInsets.all(16),
+      itemCount: gameState.cards.length,
+      itemBuilder: (context, index) {
+        final card = gameState.cards[index];
+        return CardWidget(
+          card: card,
+          onTap: () => gameState.flipCard(card),
+        );
+      },
+    );
+  }
+}
